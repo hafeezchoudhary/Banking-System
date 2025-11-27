2c3dc4f47aecfa43ac180d8de897a0cad375a38c
Hafeez Choudhary <choudharyhafeez2513@gmail.com>
2025-11-27 13:06:07 +0530

    updated

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://enbankingsystem.netlify.app/api",
 });
 
 API.interceptors.request.use((req) => {
